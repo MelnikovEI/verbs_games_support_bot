c8f058907b20c1621609256b756e0662b76c1039
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-05-04 17:46:24 +0300

    exclude excess comments

--- telegram_logs_handler.py
+++ telegram_logs_handler.py
@@ -7,7 +7,6 @@ class TelegramLogsHandler(logging.Handler):
         self.chat_id = chat_id
         self.tg_bot = tg_bot
 
-    # @retry(telegram.error.NetworkError, delay=1, backoff=2, tries=10, max_delay=30, logger=logger)
     def emit(self, record):
         log_entry = self.format(record)
         self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
